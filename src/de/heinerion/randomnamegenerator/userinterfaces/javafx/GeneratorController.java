17a83126c6b70d9a2f1f48079024e38abc54558c
Heinerion <phe0nix@heinerion.de>
2016-02-03 20:19:00 +0100

    Implement basic FX interface based on a friends work
    #1 Create a JavaFX user interface

--- src/de/heinerion/randomnamegenerator/userinterfaces/javafx/GeneratorController.java
+++ src/de/heinerion/randomnamegenerator/userinterfaces/javafx/GeneratorController.java
@@ -1,13 +1,49 @@
 package de.heinerion.randomnamegenerator.userinterfaces.javafx;
 
+import de.heinerion.randomnamegenerator.Gender;
+import de.heinerion.randomnamegenerator.generators.NameGenerator;
+import de.heinerion.randomnamegenerator.generators.PreFilledNameGenerator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GeneratorController implements Initializable {
+  private final NameGenerator nameGenerator = new PreFilledNameGenerator();
+
+  @FXML
+  private Spinner<Integer> forenameSpinner;
+
+  @FXML
+  private Spinner<Integer> surnameSpinner;
+
+  @FXML
+  private Text result;
+
+  @FXML
+  private ComboBox<Gender> genderBox = new ComboBox<>();
+
+  @FXML
+  private void handleSubmitButtonAction(ActionEvent event) {
+    nameGenerator.setNumberOfForenames(forenameSpinner.getValue());
+    nameGenerator.setNumberOfSurnames(surnameSpinner.getValue());
+
+    Gender gender = genderBox.getValue();
+    nameGenerator.setGender(gender == null ? Gender.MALE : gender);
+
+    String randomName = nameGenerator.generateRandomName();
+    result.setText(randomName);
+  }
+
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
+    // TODO translate keys
 
+    genderBox.getItems().addAll(Gender.values());
   }
 }
